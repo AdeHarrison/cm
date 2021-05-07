89d21dba39170bf39ed5bf7d27457d00fa2f8275
ade <adrian.harrison.uk@gmail.com>
2021-05-07 15:09:10 +0100

    Initial commit

---
+++ src/main/java/com/waracle/cakemanager2/CakeManager2Application.java
@@ -0,0 +1,13 @@
+package com.waracle.cakemanager2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CakeManager2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CakeManager2Application.class, args);
+	}
+
+}
