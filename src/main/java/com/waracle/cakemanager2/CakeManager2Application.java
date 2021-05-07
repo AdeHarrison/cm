1a9f92f87bee0a7a3bd0ccf2628df60e6c827647
ade <adrian.harrison.uk@gmail.com>
2021-05-07 21:25:23 +0100

    after swagger

--- src/main/java/com/waracle/cakemanager2/CakeManager2Application.java
+++ src/main/java/com/waracle/cakemanager2/CakeManager2Application.java
@@ -2,12 +2,21 @@ package com.waracle.cakemanager2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class CakeManager2Application {
+    public static void main(String[] args) {
+        SpringApplication.run(CakeManager2Application.class, args);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(CakeManager2Application.class, args);
-	}
-
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2);
+    }
 }
