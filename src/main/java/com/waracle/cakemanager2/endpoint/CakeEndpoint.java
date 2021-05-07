be74efeef6b2fbc1db263b731e1703ea4e923af8
ade <adrian.harrison.uk@gmail.com>
2021-05-07 22:07:21 +0100

    after download

--- src/main/java/com/waracle/cakemanager2/endpoint/CakeEndpoint.java
+++ src/main/java/com/waracle/cakemanager2/endpoint/CakeEndpoint.java
@@ -1,18 +1,26 @@
 package com.waracle.cakemanager2.endpoint;
 
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.waracle.cakemanager2.dto.CakeDTO;
 import com.waracle.cakemanager2.entity.Cake;
 import com.waracle.cakemanager2.service.impl.CakeServiceImpl;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Arrays;
-import java.util.List;
 
 @RestController
 @RequestMapping(path = "/")
 public class CakeEndpoint {
+    private static final ObjectMapper mapper = new ObjectMapper();
 
     private final CakeServiceImpl cakeService;
 
@@ -21,40 +29,27 @@ public class CakeEndpoint {
     }
 
     @GetMapping
-    public List<Cake> getAllCakes() {
+    public List<Cake> getCakes() {
         return cakeService.getAllCakes();
     }
 
-    //    @GetMapping(path = "/cakes")
-//    public ResponseEntity<Resource> downloadAllCakes() throws JsonProcessingException {
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
-//        headers.add("Pragma", "no-cache");
-//        headers.add("Expires", "0");
-//
-//        String cakesAsString = transformJsonToHumanReadable(getAllCakes());
-//        InputStreamResource resource = new InputStreamResource(new ByteArrayInputStream(cakesAsString.getBytes()));
-//        ByteArrayResource resource2 = new ByteArrayResource(cakesAsString.getBytes());
-//
-//        return ResponseEntity.ok()
-//                .headers(headers)
-//                .contentLength(cakesAsString.length())
-//                .contentType(MediaType.APPLICATION_OCTET_STREAM)
-//                .body(resource2);
-//    }
+    @GetMapping("/files")
+    public ResponseEntity<byte[]> getFile() {
+        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=cakes.json")
+                .body("lksjdflskdfjl".getBytes(StandardCharsets.UTF_8));
+    }
+
     @GetMapping(path = "/cakes")
-    public List<String> downloadAllCakes() {
-        return Arrays.asList("cake 11", "cake 12", "cake 13");
+    public ResponseEntity<byte[]> downloadCakes() throws JsonProcessingException {
+        String cakes = mapper.writeValueAsString(getCakes());
+        return ResponseEntity.ok()
+
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=cakes.json")
+                .body(cakes.getBytes(StandardCharsets.UTF_8));
     }
 
     @PostMapping(path = "/cakes")
     public Cake createCake(@RequestBody CakeDTO cakeDTO) {
         return cakeService.createCake(cakeDTO);
-    }
-
-    private String transformJsonToHumanReadable(List<Cake> allCakes) throws JsonProcessingException {
-        ObjectMapper mapper = new ObjectMapper();
-
-        return mapper.writeValueAsString(allCakes);
     }
 }
