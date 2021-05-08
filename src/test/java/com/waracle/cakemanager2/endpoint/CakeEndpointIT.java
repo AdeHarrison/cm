a16620401ca869d17daead629292deeaeff98445
ade <adrian.harrison.uk@gmail.com>
2021-05-08 15:55:24 +0100

    shouldRejectWith500WhenTitleAlreadyExists

--- src/test/java/com/waracle/cakemanager2/endpoint/CakeEndpointIT.java
+++ src/test/java/com/waracle/cakemanager2/endpoint/CakeEndpointIT.java
@@ -28,16 +28,17 @@ class CakeEndpointIT {
     private CakeRepository cakeRepository;
 
     @BeforeEach()
-    public void setup(){
+    public void setup() {
         cakeRepository.deleteAll();
     }
 
     @Test
     public void shouldRetrieveAllCakesSuccessfully() {
-        int expectedSize = 3;
+        int expectedSize = 2;
 
         loadTestCakes();
 
+        // @formatter:off
         List<Map<String, Object>> actual =
                     given()
                         .when()
@@ -51,6 +52,7 @@ class CakeEndpointIT {
                             .body()
                                 .as(new TypeRef<List<Map<String, Object>>>() {
                         });
+        // @formatter:on
 
         assertThat(actual.size(), equalTo(expectedSize));
     }
@@ -61,6 +63,7 @@ class CakeEndpointIT {
         String testCakeBody = "{\"title\": \"title1\", \"description\": \"description1\", \"image\": \"image1\"}";
         CakeDTO expected = new CakeDTO(6, "title1", "description1", "image1");
 
+        // @formatter:off
         String bodyAsString =
                 given()
                     .when()
@@ -75,20 +78,43 @@ class CakeEndpointIT {
                         .extract()
                             .body()
                                 .asString();
+        // @formatter:on
 
         CakeDTO actual = mapper.readValue(bodyAsString, CakeDTO.class);
 
         assertThat(actual, equalTo(expected));
     }
 
-    //todo test utils class
-    private void loadTestCakes() {
-        cakeRepository.save(new Cake("Lemon cheesecake", "A cheesecake made of lemon",
-                "https://s3-eu-west-1.amazonaws.com/s3.mediafileserver.co.uk/carnation/WebFiles/RecipeImages/lemoncheesecake_lg.jpg"));
-        cakeRepository.save(new Cake("victoria sponge", "sponge with jam",
-                "http://www.bbcgoodfood.com/sites/bbcgoodfood.com/files/recipe_images/recipe-image-legacy-id--1001468_10.jpg"));
-        cakeRepository.save(new Cake("Carrot cake", "Bugs bunnys favourite",
-                "http://www.villageinn.com/i/pies/profile/carrotcake_main1.jpg"));
+    @Test
+    public void shouldRejectWith500WhenTitleAlreadyExists() {
+        String testCakeBody = "{\"title\": \"Lemon cheesecake\", \"description\": \"description1\", \"image\": \"image1\"}";
+
+        loadTestCake1();
+
+        // @formatter:off
+        given()
+            .when()
+                .contentType(ContentType.JSON)
+                .body(testCakeBody)
+                .post(URL + "/cakes")
+            .then()
+                .statusCode(500);
+        // @formatter:on
     }
 
+    //todo move below test utils class?
+    private void loadTestCakes() {
+        loadTestCake1();
+        loadTestCake2();
+    }
+
+    private void loadTestCake1() {
+        cakeRepository.save(new Cake("Lemon cheesecake", "A cheesecake made of lemon",
+                "https://s3-eu-west-1.amazonaws.com/s3.mediafileserver.co.uk/carnation/WebFiles/RecipeImages/lemoncheesecake_lg.jpg"));
+    }
+
+    private void loadTestCake2() {
+        cakeRepository.save(new Cake("victoria sponge", "sponge with jam",
+                "http://www.bbcgoodfood.com/sites/bbcgoodfood.com/files/recipe_images/recipe-image-legacy-id--1001468_10.jpg"));
+    }
 }
